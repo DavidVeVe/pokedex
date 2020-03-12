2a0289ac1412b97321ce7a08bda300338ea67efa
Armando Velazquez <davidarmando.gdl@live.com>
2020-03-11 21:18:58 -0600

    Updates

---
+++ precache-manifest.cb9282cea3abcea896a06e0efa1b12de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6defbaea364f35eccb0af2baf63918b",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "02515e039f6904636fe7",
+    "url": "/pokedex/static/css/main.6784684c.chunk.css"
+  },
+  {
+    "revision": "42d6ea41b16f9bb44f10",
+    "url": "/pokedex/static/js/2.fa618434.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokedex/static/js/2.fa618434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02515e039f6904636fe7",
+    "url": "/pokedex/static/js/main.abcdab28.chunk.js"
+  },
+  {
+    "revision": "e9c8d32f045e9b89c48b",
+    "url": "/pokedex/static/js/runtime-main.54804aa2.js"
+  }
+]);
